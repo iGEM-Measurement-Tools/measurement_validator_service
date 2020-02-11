dd588de07ccd76da0a2663f669df45e1a1b5d698
Keoni Gandall <koeng101@gmail.com>
2020-02-11 10:30:49 -0800

    Removed value for index

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ Bootstrap(app)
 
 @app.route('/')
 def index():
-    return render_template("index.html",value=data)
+    return render_template("index.html")
 
 if __name__ == '__main__':
     app.run(debug=True)
